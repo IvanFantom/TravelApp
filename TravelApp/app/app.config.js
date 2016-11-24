fc026d63d5cfdb0d32fc2da551a6aff92b7d34e0
Ivan Zakhartchouk <ivan.zakhartchouk@itechart-group.com>
2016-11-24 18:49:17 +0300

    origin

---
+++ TravelApp/app/app.config.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .config(routesConfig);
+
+    routesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function routesConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('travel-app', {
+                url: '/',
+                templateUrl: 'app/components/travel-app.html',
+                controller: 'TravelAppController as travelApp'
+            });
+
+        $urlRouterProvider.otherwise('/');
+    }
+})();
---
+++ TravelApp/app/components/travel-app.controller.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('TravelAppController', TravelAppController);
+
+    TravelAppController.$inject = ['$state'];
+
+    function TravelAppController($state) {
+        var ctrl = this;
+
+
+    }
+})();
